5831184b87de658b2e3841e2d66f02aeb237e17a
Jakob Richter <103637279+richterjakob@users.noreply.github.com>
2022-05-24 10:49:21 -0700

    Inverse problem 0d (#2)

---
+++ svsuperestimator/model/_multifidelitymodel.py
@@ -0,0 +1,19 @@
+"""This module holds the MultiFidelityModel class."""
+from ..io import SimVascularProject
+from . import ZeroDModel
+
+
+class MultiFidelityModel:
+    """Multi-fidelity model.
+
+    This class contains attributes and methods to save and modify
+    multi-fidelity models.
+    """
+
+    def __init__(self, project: SimVascularProject):
+        """Create a new MultiFidelityModel instance.
+
+        Args:
+            project: Project object to extract the model parameters from.
+        """
+        self.zerodmodel = ZeroDModel(project)
