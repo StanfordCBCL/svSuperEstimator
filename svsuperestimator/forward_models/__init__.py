afef6a4fb4ec7896c39988e4663c5d5cc0886a90
Jakob <jakob.richter@tum.de>
2022-07-18 14:09:29 -0700

    Add forward model subclass

---
+++ svsuperestimator/forward_models/__init__.py
@@ -0,0 +1,6 @@
+from ._forward_model import ForwardModel
+from ._windkessel_distal_proximal_ratio import (
+    WindkesselDistalToProximalResistance0D,
+)
+
+__all__ = ["ForwardModel", "WindkesselDistalToProximalResistance0D"]
