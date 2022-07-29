9d540e68fb0fab88780428951f214a898af00ee8
Jakob <jakob.richter@tum.de>
2022-07-29 16:14:15 -0700

    Clean-up statutils

--- svsuperestimator/problems/statutils.py
+++ svsuperestimator/problems/statutils.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import numpy as np
 
 from scipy.stats import gaussian_kde
@@ -8,23 +9,107 @@ from pqueens.utils.pdf_estimation import (
 )
 
 
-def kernel_density_estimation_2d(
-    x, y, bounds, weights, num_points_per_axis=1000, bw_method="scott"
-):
+def particle_wmean(x: np.ndarray, y: np.ndarray, weights: np.ndarray):
+    """Calculate weighted mean of particles.
 
-    linspace_x = np.linspace(bounds[0][0], bounds[0][1], num_points_per_axis)
-    linspace_y = np.linspace(bounds[1][0], bounds[1][1], num_points_per_axis)
+    Args:
+        x: X-coordinates of particles.
+        y: Y-coordinates of particles.
+        weights: Weights of particles.
+    """
+    return np.array(
+        [np.average(x, weights=weights), np.average(y, weights=weights)]
+    )
+
+
+def particle_map(x: np.ndarray, y: np.ndarray, weights: np.ndarray):
+    """Calculate maximum a posteriori (MAP) of particles.
+
+    Args:
+        x: X-coordinates of particles.
+        y: Y-coordinates of particles.
+        weights: Weights of particles.
+    """
+    return np.array([x[np.argmax(weights)], y[np.argmax(weights)]])
+
+
+def particle_covmat(x: np.ndarray, y: np.ndarray, weights: np.ndarray):
+    """Calculate covariance matrix of particles.
+
+    Args:
+        x: X-coordinates of particles.
+        y: Y-coordinates of particles.
+        weights: Weights of particles.
+    """
+    return np.cov([x, y], aweights=weights)
+
+
+def gaussian_kde_1d(
+    x: np.ndarray,
+    weights: np.ndarray,
+    bounds: tuple[float, float],
+    num: int = 100,
+):
+    """Calculate 1d kernel-density estimate.
+
+    Args:
+        x: Coordinates of particles.
+        weights: Weights of particles.
+        bounds: Bounds for kernel-density estimation.
+        num: Number of points for kernel density estimate
+
+    Returns:
+        kde_x: Coordinates of kernel density estimate.
+        kde: Kernel density estimate.
+        bandwidth: Optimized bandwidth.
+    """
+    lin_x = np.linspace(bounds[0], bounds[1], num)
+    bandwidth = estimate_bandwidth_for_kde(
+        x,
+        weights=weights,
+        min_samples=bounds[0],
+        max_samples=bounds[1],
+        kernel="gaussian",
+    )
+    kde, kde_x = estimate_pdf(
+        x,
+        weights=weights,
+        kernel_bandwidth=bandwidth,
+        support_points=lin_x,
+        kernel="gaussian",
+    )
+    return kde_x.flatten(), kde.flatten(), bandwidth
+
+
+def gaussian_kde_2d(
+    x: np.ndarray,
+    y: np.ndarray,
+    weights: np.ndarray,
+    bounds: tuple[tuple[float, float], tuple[float, float]],
+    num=100,
+):
+    """Calculate 2d kernel-density estimate.
+
+    Args:
+        x: X-coordinates of particles.
+        y: Y-coordinates of particles.
+        weights: Weights of particles.
+        bounds: Bounds for kernel-density estimation.
+        num: Number of points for kernel density estimate
+
+    Returns:
+        kde_x: X-coordinates of kernel density estimate.
+        kde_y: Y-coordinates of kernel density estimate.
+        kde: Kernel density estimate.
+        bandwidth: Optimized bandwidth.
+    """
+    linspace_x = np.linspace(bounds[0][0], bounds[0][1], num)
+    linspace_y = np.linspace(bounds[1][0], bounds[1][1], num)
     grid_x, grid_y = np.meshgrid(linspace_x, linspace_y)
     grid_points = np.array([grid_x.ravel(), grid_y.ravel()])
 
-    kernel = gaussian_kde([x, y], weights=weights, bw_method=bw_method)
-    kde = kernel(grid_points).reshape(num_points_per_axis, -1)
+    kernel = gaussian_kde([x, y], weights=weights, bw_method="scott")
+    kde = kernel(grid_points).reshape(num, -1)
     bandwidth = kernel.factor
 
     return linspace_x, linspace_y, kde, bandwidth
-
-
-def kernel_density_estimation_1d(x, weights, bounds):
-    bandwidth = estimate_bandwidth_for_kde(x, weights=weights, min_samples=bounds[0], max_samples=bounds[1])
-    kde, support_points = estimate_pdf(x, weights=weights)
-    return support_points.flatten(), kde.flatten(), bandwidth
