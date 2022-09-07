15429915f57848a5cb04963cff6637d2fd397739
Jakob Richter <103637279+richterjakob@users.noreply.github.com>
2022-09-07 10:29:17 -0700

    Fix init

--- svsuperestimator/iterators/__init__.py
+++ svsuperestimator/iterators/__init__.py
@@ -1,9 +1,7 @@
-from ._grid_iterator import GridIterator
 from ._grid_likelihood_iterator import GridLikelihoodIterator
 from ._smc_iterator import SmcIterator
 
 __all__ = [
     "SmcIterator",
-    "GridIterator",
     "GridLikelihoodIterator",
 ]
