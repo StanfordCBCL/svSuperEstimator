36865ba317c37f08344a1fb779fc3f1e3de9efc4
Jakob Richter <103637279+richterjakob@users.noreply.github.com>
2022-09-07 10:21:46 -0700

    Delete unused iterators

--- svsuperestimator/iterators/__init__.py
+++ svsuperestimator/iterators/__init__.py
@@ -1,11 +1,9 @@
 from ._grid_iterator import GridIterator
 from ._grid_likelihood_iterator import GridLikelihoodIterator
-from ._optimization_iterator import OptimizationIterator
 from ._smc_iterator import SmcIterator
 
 __all__ = [
     "SmcIterator",
     "GridIterator",
-    "OptimizationIterator",
     "GridLikelihoodIterator",
 ]
