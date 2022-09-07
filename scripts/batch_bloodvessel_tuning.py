d8e39efe15b87093fe450a6e9858ab61f3894dae
Jakob Richter <103637279+richterjakob@users.noreply.github.com>
2022-09-07 11:56:29 -0700

    Add override falg to batch bv tuning

--- scripts/batch_bloodvessel_tuning.py
+++ scripts/batch_bloodvessel_tuning.py
@@ -8,10 +8,29 @@ from rich import print
 @click.argument("model_folder")
 @click.argument("centerline_folder")
 @click.option("--num_procs", default=1, help="Number of processors to use.")
-def main(model_folder, centerline_folder, num_procs):
+@click.option(
+    "--override",
+    is_flag=True,
+    default=False,
+)
+def main(model_folder, centerline_folder, num_procs, override):
     for model in [
         n for n in os.listdir(model_folder) if not n.startswith(".")
     ]:
+        if not override and os.path.exists(
+            os.path.join(
+                model_folder,
+                model,
+                "ParameterEstimation",
+                "BloodVesselTuning",
+                "solver_0d.in",
+            )
+        ):
+            print(
+                f"Optimization for model {model} [bold orange]exists[/bold orange]"
+            )
+            continue
+
         config = {
             "project": os.path.join(model_folder, model),
             "global": {"num_procs": num_procs},
