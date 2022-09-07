54d12cb189e6c283cb67b9799b5727de8790b3f2
Jakob Richter <103637279+richterjakob@users.noreply.github.com>
2022-09-07 10:17:09 -0700

    Delete forward models

--- svsuperestimator/solver/__init__.py
+++
@@ -1,8 +0,0 @@
-"""svSuperEstimator's solver subpackage.
-
-Contains everything related to solving cardiovascular models.
-"""
-from ._threedsolver import ThreeDSolver
-from ._zerodsolver import ZeroDSolver
-
-__all__ = ["ZeroDSolver", "ThreeDSolver"]
