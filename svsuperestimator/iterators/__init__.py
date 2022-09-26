d70beeac053e993be9847007cb1b9a06f29cde7a
Jakob Richter <103637279+richterjakob@users.noreply.github.com>
2022-09-26 10:50:29 -0700

    Update queens and use sqlite

--- svsuperestimator/iterators/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._grid_likelihood_iterator import GridLikelihoodIterator
-from ._smc_iterator import SmcIterator
-
-__all__ = [
-    "SmcIterator",
-    "GridLikelihoodIterator",
-]
--- svsuperestimator/tasks/windkessel_tuning.py
+++ svsuperestimator/tasks/windkessel_tuning.py
@@ -1,16 +1,19 @@
 from __future__ import annotations
 
+import json
 import os
 import pickle
 from datetime import datetime
+from tempfile import TemporaryDirectory
 
 import numpy as np
 import orjson
 import pandas as pd
+from pqueens.main import run
 from rich.console import Console
 from svzerodsolver import runnercpp
 
-from .. import iterators, reader, visualizer
+from .. import reader, visualizer
 from ..reader import utils as readutils
 from . import plotutils, statutils, taskutils
 from .task import Task
@@ -82,7 +85,7 @@ class WindkesselTuning(Task):
 
         # Setup the iteratior
         self.log("Setup tuning process")
-        iterator = iterators.SmcIterator(
+        smc_runner = SMCRunner(
             forward_model=forward_model,
             y_obs=y_obs,
             output_dir=self.output_folder,
@@ -96,7 +99,7 @@ class WindkesselTuning(Task):
 
         # Add random variables for each parameter
         for i in range(len(theta_obs)):
-            iterator.add_random_variable(
+            smc_runner.add_random_variable(
                 f"k{i}",
                 "uniform",
                 lower_bound=self._THETA_RANGE[0],
@@ -105,7 +108,7 @@ class WindkesselTuning(Task):
 
         # Run the iterator
         self.log("Starting tuning process")
-        iterator.run()
+        smc_runner.run()
 
         # Save parameters to file
         self.database["timestamp"] = datetime.now().strftime(
@@ -524,3 +527,173 @@ class _Forward_Model:
         ]
 
         return np.array([p_inlet.min(), p_inlet.max(), *q_outlet_mean])
+
+class SMCRunner:
+    """Sequentia-Monte-Carlo iterator for static models."""
+
+    def __init__(
+        self,
+        forward_model,
+        y_obs: np.ndarray,
+        output_dir=None,
+        num_procs=1,
+        **kwargs
+    ):
+        """Create a new SmcIterator instance.
+
+        Args:
+            forward_model: Forward model.
+            y_obs: Matrix with row-wise observation vectors.
+            output_dir: Output directory.
+            num_procs: Number of parallel processes.
+            kwargs: Optional parameters
+                * `database_address`: Address to the database for QUEENS.
+                * `num_particles`: Number of particles for SMC.
+                * `resampling_threshold`: Resampling threshold for SMC.
+                * `num_rejuvenation_steps`: Number of rejuvenation steps SMC.
+
+        """
+        self._y_obs = y_obs
+        self._config = {
+            "global_settings": {
+                "output_dir": output_dir,
+                "experiment_name": "results",
+            },
+            "database": {
+                "name": "database",
+                "type": "sqlite",
+            },
+            "forward_model": {
+                "type": "simulation_model",
+                "interface": "interface",
+                "parameters": "parameters",
+            },
+            "interface": {
+                "type": "direct_python_interface",
+                "external_python_module_function": forward_model.evaluate,
+                "num_workers": num_procs,
+            },
+            "parameters": {"random_variables": {}},
+            "method": {
+                "method_name": "smc_chopin",
+                "method_options": {
+                    "seed": 42,
+                    "num_particles": kwargs["num_particles"],
+                    "resampling_threshold": kwargs["resampling_threshold"],
+                    "resampling_method": "systematic",
+                    "feynman_kac_model": "adaptive_tempering",
+                    "waste_free": True,
+                    "num_rejuvenation_steps": kwargs["num_rejuvenation_steps"],
+                    "model": "model",
+                    "max_feval": 9e99,
+                    "result_description": {
+                        "write_results": True,
+                        "plot_results": False,
+                    }
+                }
+            },
+            "model":{
+                "type": "gaussian",
+                "forward_model": "forward_model",
+                "output_label": "y_obs",
+                "coordinate_labels": [],
+                "noise_type": kwargs["noise_type"],
+                "noise_value": kwargs["noise_value"],
+                "experimental_file_name_identifier": "*.csv",
+                "experimental_csv_data_base_dir": None,
+                "parameters": "parameters",
+            }
+        }
+    
+    def add_random_variable(
+        self,
+        name: str,
+        dist_type: str,
+        **kwargs: dict,
+    ):
+        """Add a new random variable to the iterator configuration.
+
+        Args:
+            label: Name of the variable.
+            dist_type: Distribution type of the variable (`normal`, `uniform`,
+                `lognormal`, `beta`)
+            options: Parameters of the distribution. For `uniform` distribution
+                specify `lower_bound` and `upper_bound`. For `normal
+                distribution specify `mean` and `covariance`. For `beta`
+                distribution specify `lower_bound`, `upper_bound`, `a`, and
+                `b`. For `lognormal` specify `normal_mean` and
+                `normal_covariance`.
+        """
+        var_config = {
+            "distribution": dist_type,
+            "type": "FLOAT",
+            "size": 1,
+            "dimension": 1,
+        }
+
+        if dist_type == "uniform":
+            var_config.update(
+                {
+                    "lower_bound": kwargs["lower_bound"],
+                    "upper_bound": kwargs["upper_bound"],
+                }
+            )
+        elif dist_type == "normal":
+            var_config.update(
+                {
+                    "mean": kwargs["mean"],
+                    "covariance": kwargs["covariance"],
+                }
+            )
+        elif dist_type == "beta":
+            var_config.update(
+                {
+                    "lower_bound": kwargs["lower_bound"],
+                    "upper_bound": kwargs["upper_bound"],
+                    "a": kwargs["a"],
+                    "b": kwargs["b"],
+                }
+            )
+        elif dist_type == "lognormal":
+            var_config.update(
+                {
+                    "normal_mean": kwargs["normal_mean"],
+                    "normal_covariance": kwargs["normal_covariance"],
+                }
+            )
+        else:
+            raise ValueError(f"Unknown distribution type {dist_type}")
+
+        self._config["parameters"]["random_variables"][name] = var_config
+
+    def run(self):
+        """Run the iterator."""
+
+        with TemporaryDirectory() as tmpdir:
+
+            # Make file for y_obs
+            target_file = os.path.join(tmpdir, "targets.csv")
+            with open(target_file, "w") as ff:
+                ff.write("y_obs\n" + "\n".join([str(t) for t in self._y_obs]))
+            self._config["model"]["experimental_csv_data_base_dir"] = tmpdir
+
+            # Set output directory to temporary directory if not set
+            if self._config["global_settings"]["output_dir"] is None:
+                self._config["global_settings"]["output_dir"] = tmpdir
+
+            with open(
+                os.path.join(
+                    self._config["global_settings"]["output_dir"],
+                    "queens_input.json",
+                ),
+                "w",
+            ) as ff:
+                json.dump(
+                    self._config,
+                    ff,
+                    indent=4,
+                    default=lambda o: "<not serializable>",
+                )
+
+            # Run queens
+            run(self._config, self._config["global_settings"]["output_dir"])
