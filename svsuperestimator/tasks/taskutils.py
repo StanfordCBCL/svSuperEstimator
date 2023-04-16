16540411f2680c7acecbf5623010cbe26a38c730
Jakob Richter <103637279+richterjakob@users.noreply.github.com>
2023-04-16 22:34:28 +0200

    Add new method for mapping centerline results

--- svsuperestimator/tasks/taskutils.py
+++ svsuperestimator/tasks/taskutils.py
@@ -244,6 +244,107 @@ def map_centerline_result_to_0d_2(
     return branchdata, times
 
 
+def map_centerline_result_to_0d_3(
+    zerod_handler: reader.SvZeroDSolverInputHandler,
+    cl_handler: reader.CenterlineHandler,
+    threed_handler: reader.SvSolverInputHandler,
+    results_handler: reader.CenterlineHandler,
+) -> Tuple[dict, np.ndarray]:
+    """Map centerine result onto 0d elements."""
+
+    # calculate cycle period
+    cycle_period = (
+        zerod_handler.boundary_conditions["INFLOW"]["bc_values"]["t"][-1]
+        - zerod_handler.boundary_conditions["INFLOW"]["bc_values"]["t"][0]
+    )
+
+    # Extract time steps
+    times = results_handler.time_steps * threed_handler.time_step_size
+
+    # Calculate start of last cycle
+    start_last_cycle = (
+        np.abs(times - (times[-1] - cycle_period))
+    ).argmin() - 1
+
+    def filter_last_cycle(data):  # type: ignore
+        if start_last_cycle == -1:
+            return data[:]
+        return data[start_last_cycle:-1]
+
+    # Extract branch information of 0D config
+    branchdata: dict = {}
+    for vessel_config in zerod_handler.vessels.values():
+        # Extract branch and segment id from name
+        name = vessel_config["vessel_name"]
+        branch_id, seg_id = name.split("_")
+        branch_id, seg_id = int(branch_id[6:]), int(seg_id[3:])
+
+        if branch_id not in branchdata:
+            branchdata[branch_id] = {}
+
+        branchdata[branch_id][seg_id] = {}
+        branchdata[branch_id][seg_id]["length"] = vessel_config[
+            "vessel_length"
+        ]
+        branchdata[branch_id][seg_id]["vessel_id"] = vessel_config["vessel_id"]
+
+    # Add start and endpoint of branches
+    for branch_id, branch in branchdata.items():
+        cl_data = cl_handler.get_branch_data(branch_id)
+
+        seg_start = 0.0
+        seg_start_index = 0
+
+        cum_len = sum([segment["length"] for segment in branch.values()])
+        if abs(cum_len - cl_data["path"][-1]) > 1e4:
+            raise RuntimeError(
+                "Indexing mismatch between 0D solver input file and "
+                "centerline. Please check that 0D config and centerline "
+                "are matching."
+            )
+
+        for seg_id in range(len(branch)):
+            segment = branch[seg_id]
+            length = segment["length"]
+            seg_end_index = (
+                np.abs(cl_data["path"] - length - seg_start)
+            ).argmin()
+            seg_end = cl_data["path"][seg_end_index]
+            segment.update(
+                {
+                    "x0": cl_data["points"][seg_start_index],
+                    "x1": cl_data["points"][seg_end_index],
+                }
+            )
+            seg_start = seg_end
+            seg_start_index = seg_end_index
+
+    # Get field data
+    for branch_id, branch in branchdata.items():
+        for seg_id in range(len(branch)):
+            segment = branch[seg_id]
+            x0 = segment["x0"]
+            x1 = segment["x1"]
+            pressure_in, flow_in = results_handler.get_values_at_node(x0)
+            pressure_out, flow_out = results_handler.get_values_at_node(x1)
+
+            segment.update(
+                {
+                    "flow_in": filter_last_cycle(flow_in),
+                    "flow_out": filter_last_cycle(flow_out),
+                    "pressure_in": filter_last_cycle(pressure_in),
+                    "pressure_out": filter_last_cycle(pressure_out),
+                }
+            )
+
+    if start_last_cycle == -1:
+        times -= times[0]
+    else:
+        times = times[start_last_cycle:-1] - np.amin(times[start_last_cycle])
+
+    return branchdata, times
+
+
 def set_initial_condition(
     zerod_handler: reader.SvZeroDSolverInputHandler, mapped_data: dict, times
 ) -> None:
@@ -261,33 +362,37 @@ def set_initial_condition(
     initial_condition = {}
     initial_condition_d = {}
     for ele1, ele2 in nodes:
-        try:
+        if ele1.startswith("branch"):
             branch_id, seg_id = ele1.split("_")
             branch_id, seg_id = int(branch_id[6:]), int(seg_id[3:])
             pressure = mapped_data[branch_id][seg_id]["pressure_out"]
             flow = mapped_data[branch_id][seg_id]["flow_out"]
-            pres, dpres = refine_with_cubic_spline_derivative(times, pressure, len(times))
-            flow, dflow = refine_with_cubic_spline_derivative(times, flow, len(times))
+            pres, dpres = refine_with_cubic_spline_derivative(
+                times, pressure, len(times)
+            )
+            flow, dflow = refine_with_cubic_spline_derivative(
+                times, flow, len(times)
+            )
             initial_condition[f"pressure:{ele1}:{ele2}"] = pres[0]
             initial_condition_d[f"pressure:{ele1}:{ele2}"] = dpres[0]
             initial_condition[f"flow:{ele1}:{ele2}"] = flow[0]
             initial_condition_d[f"flow:{ele1}:{ele2}"] = dflow[0]
-        except ValueError:
-            pass
 
-        try:
+        if ele2.startswith("branch"):
             branch_id, seg_id = ele2.split("_")
             branch_id, seg_id = int(branch_id[6:]), int(seg_id[3:])
             pressure = mapped_data[branch_id][seg_id]["pressure_in"]
             flow = mapped_data[branch_id][seg_id]["flow_in"]
-            pres, dpres = refine_with_cubic_spline_derivative(times, pressure, len(times))
-            flow, dflow = refine_with_cubic_spline_derivative(times, flow, len(times))
+            pres, dpres = refine_with_cubic_spline_derivative(
+                times, pressure, len(times)
+            )
+            flow, dflow = refine_with_cubic_spline_derivative(
+                times, flow, len(times)
+            )
             initial_condition[f"pressure:{ele1}:{ele2}"] = pres[0]
             initial_condition_d[f"pressure:{ele1}:{ele2}"] = dpres[0]
             initial_condition[f"flow:{ele1}:{ele2}"] = flow[0]
             initial_condition_d[f"flow:{ele1}:{ele2}"] = dflow[0]
-        except ValueError:
-            pass
 
         if ele2.startswith("RCR"):
             initial_condition[f"pressure_c:{ele2}"] = (
@@ -300,6 +405,14 @@ def set_initial_condition(
                 - bcs[ele2]["bc_values"]["Rp"]
                 * initial_condition_d[f"flow:{ele1}:{ele2}"]
             )
+        if ele2.startswith("RESISTANCE"):
+            initial_condition[f"pressure_c:{ele2}"] = initial_condition[
+                f"pressure:{ele1}:{ele2}"
+            ]
+            initial_condition_d[f"pressure_c:{ele2}"] = initial_condition_d[
+                f"pressure:{ele1}:{ele2}"
+            ]
+
     vessel_id_map = zerod_handler.vessel_id_to_name_map
 
     for junction_name, junction in zerod_handler.junctions.items():
@@ -307,11 +420,11 @@ def set_initial_condition(
             for i, outlet_vessel in enumerate(junction["outlet_vessels"]):
                 ovessel_name = vessel_id_map[outlet_vessel]
                 initial_condition[
-                        f"flow_{i}:{junction_name}"
-                    ] = initial_condition[f"flow:{junction_name}:{ovessel_name}"]
+                    f"flow_{i}:{junction_name}"
+                ] = initial_condition[f"flow:{junction_name}:{ovessel_name}"]
                 initial_condition_d[
-                        f"flow_{i}:{junction_name}"
-                    ] = initial_condition_d[f"flow:{junction_name}:{ovessel_name}"]
+                    f"flow_{i}:{junction_name}"
+                ] = initial_condition_d[f"flow:{junction_name}:{ovessel_name}"]
 
     zerod_handler.data["initial_condition"] = initial_condition
     zerod_handler.data["initial_condition_d"] = initial_condition_d
